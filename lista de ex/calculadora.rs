cf54fe0b963473537581fbaff38905839bc996ed
poledna0 <henrique.poledna@gmail.com>
2024-08-22 13:55:32 -0300

    feat: sim

--- lista de ex/calculadora.rs
+++ lista de ex/calculadora.rs
@@ -1,6 +1,47 @@
-
 fn main() {
-    for i in 5..11{
-        println!("{}",i);
+    let mut buffer = String::new();
+    let mut entrada1 = String::new();
+    let mut entrada2 = String::new();
+    let mut resultado:u32 = 0;
+    
+    println!("Digite [1] para soma");
+    println!("Digite [2] para subtração");
+    println!("Digite [3] para multiplicação");
+    println!("Digite [4] para divisão");
+    
+    std::io::stdin()
+        .read_line(&mut buffer)
+        .expect("ERRO A LER A LINHA!!");
+    let escolha_de_numero:u8= buffer
+        .trim()
+        .parse()
+        .expect("ERRO A CONVERTER!!");
+    println!("Digite o primeiro numero: ");
+    std::io::stdin()
+        .read_line(&mut entrada1)
+        .expect("ERRO A LER A LINHA!!");
+    let escolha_de_numero1:u32= entrada1
+        .trim()
+        .parse()
+        .expect("ERRO A CONVERTER!! O PRIMEIRO NUMERO");
+    println!("Digite o segundo numero:  ");
+    std::io::stdin()
+        .read_line(&mut entrada2)
+        .expect("ERRO A LER A LINHA!!");
+    let escolha_de_numero2:u32= entrada2
+        .trim()
+        .parse()
+        .expect("ERRO A CONVERTER!! O SEGUNDO NUMERO");
+    
+    match escolha_de_numero{
+        1 => resultado = escolha_de_numero1 + escolha_de_numero2,
+        2 => resultado = escolha_de_numero1 - escolha_de_numero2,
+        3 => resultado = escolha_de_numero1 * escolha_de_numero2,
+        4 => resultado = escolha_de_numero1 / escolha_de_numero2,
+        _ => panic!("NUMERO INVALIDO"),
     }
+    println!("O RESULTADO É: {}",resultado)
 }
+
+    
+
