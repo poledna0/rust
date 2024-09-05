636ba29a0b9f6b3952fbf21c6112cd8401ca09f6
Henrique <59295005+poledna0@users.noreply.github.com>
2024-09-05 12:25:24 -0300

    Update ex3.rs

--- lista de ex/ex3.rs
+++ lista de ex/ex3.rs
@@ -1,3 +1,5 @@
+//Implemente um código que, através de um laço de repetição (while, loop ou for), encontre
+//o menor número (qualquer tipo numérico) de um vetor. Ao final, mostre o valor encontrado.
 fn main() {
     let mv = vec![2,5,4,3,5,7,5,3,1,5,7,6,4,3,7,9,72,5];
     let mut mn:u8 = mv[0];
