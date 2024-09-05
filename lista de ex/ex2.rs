2535ccf4b21b8a75421c0be3cc4865a925f4ae4b
Henrique <59295005+poledna0@users.noreply.github.com>
2024-09-05 12:25:02 -0300

    Update ex2.rs

--- lista de ex/ex2.rs
+++ lista de ex/ex2.rs
@@ -1,6 +1,8 @@
+//Implemente um código que, através de um laço de repetição (while, loop ou for), encontre
+//o maior número (qualquer tipo numérico) de um vetor. Ao final, mostre o valor encontrado.
 fn main() {
     let mv = vec![2,5,4,3,5,7,5,3,2,5,7,6,4,3,7,9,72,5];
-    let mut mm:u8 = 0;
+    let mut mm:u8 = mv[0];
     for n in mv{
         if n > mm{
             mm = n
