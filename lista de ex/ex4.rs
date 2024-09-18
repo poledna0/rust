dc731a5910b3f1d023135375f7fbad17d0010b55
Henrique <59295005+poledna0@users.noreply.github.com>
2024-09-18 13:30:13 -0300

    Update ex4.rs

--- lista de ex/ex4.rs
+++ lista de ex/ex4.rs
@@ -1,44 +1,29 @@
 //Implemente um código que para verificar se um triângulo é equilátero, isósceles ou
 //escaleno. Solicite do usuário os três lados do triângulo.
 fn main() {
-    let mut l1 = String::new();
-    let mut l2 = String::new();
-    let mut l3 = String::new();
-
-    //variavel1 as f32 variavel2 as f32, apenas no lugar, n muda a variavel
-    // let mut variavel_float = variavel as f32 :)
+    let mut lado_um = String::new();
+    let mut lado_dois = String::new();
+    let mut lado_tres = String::new();
     
     println!("Digite o primeiro lado do tringulo: ");
     
     std::io::stdin()
-        .read_line(&mut l1)
+        .read_line(&mut lado_um)
         .expect("ERRO A LER A LINHA!!");
-    let l12:u8= l1
-        .trim()
-        .parse()
-        .expect("ERRO A CONVERTER!! O PRIMEIRO NUMERO");
-        
+
     println!("Digite o segundo lado do tringulo");
     std::io::stdin()
-        .read_line(&mut l2)
+        .read_line(&mut lado_dois)
         .expect("ERRO A LER A LINHA!!");
-    let l22:u8= l2
-        .trim()
-        .parse()
-        .expect("ERRO A CONVERTER!! O SEGUNDO NUMERO");
-        
+
     println!("Digite o terceiro lado do tringulo");
     std::io::stdin()
-        .read_line(&mut l3)
+        .read_line(&mut lado_tres)
         .expect("ERRO A LER A LINHA!!");
-    let l32:u8= l3
-        .trim()
-        .parse()
-        .expect("ERRO A CONVERTER!! O TERCEIRO NUMERO");
-        
-    if l1 == l2 && l2 == l3 {
+
+    if lado_um == lado_dois && lado_dois == lado_tres {
         println!("O triangulo e equilatero.");
-    } else if l1 == l2 || l1 == l3 || l2 == l3 {
+    } else if lado_um == lado_dois || lado_um == lado_tres || lado_dois == lado_tres {
         println!("O triangulo e isosceles.");
     } else {
         println!("O triangulo e escaleno.");
